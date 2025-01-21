a586a74a5f3cc4229caf8fd15c81277daeeee2af
MSulemanY <suleman501@gmail.com>
2025-01-21 07:29:31 -0500

    Done the second testcase

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -27,10 +27,11 @@ class TestAccountModel(TestCase):
         db.session.close()
     def setUp(self):
         """Truncate the tables"""
-
+        db.session.query(Account).delete()
+        db.session.commit()
     def tearDown(self):
         """Remove the session"""
-
+        db.session.remove()
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
@@ -41,3 +42,10 @@ class TestAccountModel(TestCase):
         account = Account(**data)
         account.create()
         self.assertEqual(len(Account.all()), 1)
+
+    def test_create_all_accounts(self):
+        """ Test creating multiple Accounts """
+        for data in ACCOUNT_DATA:
+            account = Account(**data)
+            account.create()
+        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))    
