69cf999c13900211d16ceda1c66ec76b9eb1c53b
MSulemanY <suleman501@gmail.com>
2025-01-30 04:47:32 -0500

    Testcases and code added

--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -47,4 +47,11 @@ class CounterTest(TestCase):
         self.assertEqual(result.status_code, status.HTTP_200_OK)
         data = result.get_json()
         self.assertEqual(data["bin"], 0)
-        
+
+    def test_delete_a_counter(self):
+        """It should delete the counter"""
+        result = self.client.post("/counters/fob")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        # Delete the counter
+        result = self.client.delete("/counters/fob")
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)        
