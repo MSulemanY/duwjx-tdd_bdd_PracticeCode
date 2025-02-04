f5f0f8e080e2d012579bdabd113b8bc14bce850a
MSulemanY <suleman501@gmail.com>
2025-02-04 12:00:50 -0500

    parameterized msg

--- labs/13_variable_substitution/features/steps/web_steps.py
+++ labs/13_variable_substitution/features/steps/web_steps.py
@@ -20,17 +20,19 @@ def step_impl(context, element_name, text_string):
     element = context.driver.find_element(By.ID, element_id)
     element.clear()
     element.send_keys(text_string)
-    
-@when('I click the "Search" button')
-def step_impl(context):
-    element = context.driver.find_element(By.ID, 'search-btn')
+
+@when('I click the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    element = context.driver.find_element(By.ID, button_id)
     element.click()
 
-@then('I should see the message "Success"')
-def step_impl(context):
-    element = context.driver.find_element(By.ID, 'flash_message')
-    assert "Success" in element.text
 
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    element = context.driver.find_element(By.ID, 'flash_message')
+    assert message in element.text
+    
 @then('I should see "Fido" in the results')
 def step_impl(context):
     element = context.driver.find_element(By.ID, 'search_results')
